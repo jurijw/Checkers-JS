4248a04e6ae9ee15985096c8ebc3171eeb97cfd3
Jurij Waite <jurijlwaite@gmail.com>
2020-02-18 01:00:12 +0100

    Added valid moves function.

--- board.js
+++ board.js
@@ -1,5 +1,6 @@
 class Board {
   constructor() {
+    this.whiteTurn = false;
     this.board = [];
     // populate the board
     for (let i = 0; i < 8; i++) {
@@ -26,6 +27,98 @@ class Board {
           this.board[j][i] = new Piece(i, j, false, false);
         }
       }
+    }
+  }
+
+  validMoves() {
+    let validMoves = {};
+    let allCapturingMoves = [];
+    let allSingleSquareMoves = [];
+
+    let vectors = undefined;
+    // Loop through all pieces and select the current players pieces
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        let piece = this.board[j][i];
+        // If the square is not empty
+        if (!piece.empty) {
+          // Get the vectors to the squares the piece can move to
+          if (piece.crowned) {
+            vectors = [
+              [-1, 1],
+              [1, 1],
+              [-1, -1],
+              [1, -1]
+            ];
+          } else if (piece.isWhite) {
+            vectors = [
+              [-1, 1],
+              [1, 1]
+            ];
+          } else {
+            vectors = [
+              [-1, -1],
+              [1, -1]
+            ];
+          }
+
+          let pieceSingleSquareMoves = []; // Array for single square moves
+          let pieceCapturingMoves = []; // Array to store capturing moves for that piece
+          for (let vector of vectors) {
+            // Calculate useful indices (positions of squares)
+            let neighborX = i + vector[0];
+            let neighborY = j + vector[1];
+            let squareBehindNeighborX = i + vector[0] * 2;
+            let squareBehindNeighborY = j + vector[1] * 2;
+
+            // Make sure the neighboring square is on the board
+            if (
+              neighborX >= 0 &&
+              neighborX < 8 &&
+              neighborY >= 0 &&
+              neighborY < 8
+            ) {
+              // Check if there is an opposite colored piece on the square
+              let neighbor = this.board[neighborY][neighborX];
+              if (!neighbor.empty && neighbor.isWhite !== piece.isWhite) {
+                // Check the square behind it
+                let squareBehindNeighbor = this.board[squareBehindNeighborY][
+                  squareBehindNeighborX
+                ];
+
+                // If its empty add this move to the capturing moves of the current piece
+                if (squareBehindNeighbor.empty) {
+                  let piecePos = [i, j];
+                  let endPos = [squareBehindNeighborX, squareBehindNeighborY];
+                  let intermediateSteps = [[neighborY, neighborX]];
+                  pieceCapturingMoves.push([endPos, intermediateSteps]);
+                  allCapturingMoves.push([
+                    piecePos,
+                    [endPos, intermediateSteps]
+                  ]);
+                }
+              } else if (neighbor.empty) {
+                let piecePos = [i, j];
+                let endPos = [neighborY, neighborX];
+                let intermediateSteps = [];
+                pieceSingleSquareMoves.push([endPos, intermediateSteps]);
+                allSingleSquareMoves.push([
+                  piecePos,
+                  [endPos, intermediateSteps]
+                ]);
+              }
+            }
+          }
+        }
+      }
+    }
+    // If capturing moves can be made, add only these to the pieces valid moves
+    if (allCapturingMoves.length > 0) {
+      return allCapturingMoves;
+    } else if (allSingleSquareMoves.length > 0) {
+      return allSingleSquareMoves;
+    } else {
+      return [];
     }
   }
 
@@ -74,7 +167,7 @@ class Board {
 
           // Outline selected piece
           if (piece.selected) {
-            fill(0, 0, 220);
+            fill(100, 220, 100);
             circle(centerX, centerY, radius, 2);
           }
         }
