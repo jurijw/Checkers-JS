8c31f072ee933f5470e3f64ed4a3e300406e0a03
Jurij Waite <jurijlwaite@gmail.com>
2020-02-19 21:31:02 +0100

    Added staticEval function and began miniMax algorithm.

--- ai.js
+++ ai.js
@@ -78,3 +78,81 @@ function simulateCaptureRecursively(board, piece, stepSequence = undefined) {
   // Return the final step sequence
   return stepSequence;
 }
+
+function staticEval(board) {
+  // Takes a board object and returns a float loosely representing the evaluation of either players
+  // chances to win. A positive outcome means white is evaluated better, a negative outcome means red
+  // Is evaluated as having a better position.
+
+  // Set up some basic values
+  let eval = 0;
+
+  let pieceVal = 1;
+  let edgePieceVal = 1.1;
+  let crownedPieceVal = 1.6;
+
+  // Loop through the board
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      // Get the square / piece in question
+      let piece = board.board[j][i];
+
+      // Make sure the square is not empty
+      if (!piece.empty) {
+        let sign = undefined;
+        if (piece.isWhite) {
+          sign = 1;
+        } else {
+          sign = -1;
+        }
+
+        // Check if the piece is crowned
+        if (piece.crowned) {
+          eval += sign * crownedPieceVal;
+        }
+        // Otherwise check if the piece is on the edge
+        else if (piece.x === 0 || piece.x === 7) {
+          eval += sign * edgePieceVal;
+        }
+        // If not just add the default piece value
+        else {
+          eval += sign * pieceVal;
+        }
+      }
+    }
+  }
+
+  // Return the evaluation
+  return eval;
+}
+
+function minimax(board, depth, whiteTurn) {
+  // Base case - max depth reached
+  // FIX: need to check for game over
+  if (depth === 0) {
+    return staticEval(board);
+  }
+
+  // Simulate all possible moves
+  let simulatedBoards = [];
+
+  let validMoves = board.validMoves;
+  let canCapture = validMoves[0];
+  // Loop through all valid moves
+  for (let moveInfo of validMoves[1]) {
+    // Disect the move info
+    let initialX = moveInfo[0][0];
+    let initialY = moveInfo[0][1];
+
+    let finalX = moveInfo[1][0][0];
+    let finalY = moveInfo[1][0][1];
+
+    let intermediateSteps = moveInfo[1][1];
+
+    // If the piece can capture loop through all sequential capturing moves.
+  }
+
+  if (whiteTurn) {
+    let maxEval = -Infinity;
+  }
+}
