c98e172c8a7c5f5425d2fe89915cc49645c94d34
Jurij Waite <jurijlwaite@gmail.com>
2020-02-19 18:27:51 +0100

    Added clone function for piece object.

--- piece.js
+++ piece.js
@@ -21,4 +21,39 @@ class Piece {
       this.crowned = true;
     }
   }
+
+  clone() {
+    // Returns a clone of the piece object with no references to any of the original pieces properties
+
+    // Clone the primitives
+    let clonedPiece = new Piece(this.x, this.y, this.isWhite, this.empty);
+    clonedPiece.crowned = this.crowned;
+    clonedPiece.selected = this.selected;
+
+    // UPGRADE: could maybe be done recurssively (currently only clones 3 layers deep)
+    let clonedValidMoves = [];
+
+    for (let moveInfo of this.validMoves) {
+      // Clone the final position
+      let clonedFinalPos = moveInfo[0].slice();
+
+      // Get and clone the intermediate steps
+      let intermediateSteps = moveInfo[1];
+      let clonedIntermediateSteps = [];
+
+      // Loop through the intermediate steps and clone each one
+      for (let intermediateStep of intermediateSteps) {
+        let clonedIntermediateStep = intermediateStep.slice();
+        clonedIntermediateSteps.push(clonedIntermediateStep);
+      }
+
+      // Push the cloned move info to the cloned valid moves array
+      let clonedMoveInfo = [clonedFinalPos, clonedIntermediateSteps];
+      clonedValidMoves.push(clonedMoveInfo);
+    }
+    // Add the cloned valid moves to the cloned piece properties
+    clonedPiece.validMoves = this.clonedValidMoves;
+
+    return clonedPiece;
+  }
 }
